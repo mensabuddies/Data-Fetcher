d2f7b8638cc423d52e178ee489490e64c402b7f8
Timo Reusch <mail@timo-reusch.de>
2022-09-20 23:13:48 +0200

    Refactor endpoints

--- mensa-api/src/main/java/com/example/mensaapi/database/repositories/MenuRepository.java
+++ mensa-api/src/main/java/com/example/mensaapi/database/repositories/MenuRepository.java
@@ -6,7 +6,13 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.time.LocalDate;
 import java.util.Optional;
+import java.util.List;
 
 public interface MenuRepository extends CrudRepository<Menu, Integer> {
+
     Optional<Menu> findMenuByDateAndMeal(LocalDate data, Meal meal);
+
+    List<Menu> findMenuByFoodProviderIdOrderByDate(int foodProviderId);
+
+    List<Menu> findMenuByFoodProviderIdAndDate(int foodProviderId, LocalDate date);
 }
